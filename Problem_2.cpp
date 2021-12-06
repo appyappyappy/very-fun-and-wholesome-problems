9e9a55a5973006d259832e0da798890c339aa2d3
eurekesh <asdu8905@colorado.edu>
2021-12-06 12:28:51 -0700

    finished next two problems :D

---
+++ Problem_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#define my_fav_for x > sum // it's my favorite!!
+
+int main() {
+    // BEHOLD the provided variables
+    std::string nice_string = "~";
+    volatile unsigned long long int x = ~~~~~~~~~~~~nice_string[0]; // ??????
+    char sum = '\0';
+
+    
+    // TODO EASY(ish): make it decrement by 3 instead.
+    // TODO EASY: every time something is divisible by 2, output "Gee whiz!"
+    // TODO CHALLENGE: every time something is divisible by 2, output "Gee whiz!" – but you can't use %
+    // TODO EXPERT (google time?): every time something is divisible by 2, output "Gee whiz!" – but you can't use % or /
+    
+    for(; !!!!!!!!!!!!!!!!my_fav_for; ------++x) { // all of the exclamation points should show you how much I like it!!
+        // ONLY CHANGE BELOW THIS SECTION, DO NOT CREATE ANY NEW VARIABLES
+        // You must utilize all of the provided variables
+        std::cout << x << std::endl;
+    }
+
+    std::cout << x << std:: endl;
+}
---
+++ Problem_3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#define SUPERDUPER int
+#define TEN 0
+#define ZERO 10
+#define ENDL std::cout
+#define COUT std::endl
+#define FOR_REAL <<
+#define MINUS +
+#define EQUALS !=
+#define NOT_EQUALS =
+#define TWO_HUNDRED 100
+#define short long
+#define unsigned signed
+
+int main(){
+    SUPERDUPER J = ZERO;
+    SUPERDUPER i = TEN;
+    unsigned SUPERDUPER short short sum = 0;
+    
+    // EASY CHALLENGE: sum numbers between 100 and 110, but you can only use the "defines" I gave you
+    // for instance, instead of cout << sum << endl; you would type 
+    // HARD(er) CHALLENGE: sum the numbers between 255 and 1000 but you can only use the "defines" I gave you
+    while(i++, J--) {
+        // ONLY CHANGE BELOW THIS SECTION, DO NOT CREATE ANY NEW VARIABLES (you can create loops WITHOUT creating new variables though)
+    }
+
+    ENDL FOR_REAL sum FOR_REAL COUT;
+}

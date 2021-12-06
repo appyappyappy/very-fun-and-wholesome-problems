02da42b6f2b9f0b750b74513d5ac0c31f74be510
eurekesh <asdu8905@colorado.edu>
2021-12-06 13:11:02 -0700

    corrected problem 3 :D

--- Problem_3.cpp
+++ Problem_3.cpp
@@ -18,10 +18,11 @@ int main(){
     unsigned SUPERDUPER short short sum = 0;
     
     // EASY CHALLENGE: sum numbers between 100 and 110, but you can only use the "defines" I gave you
-    // for instance, instead of cout << sum << endl; you would type 
+    // for instance, instead of cout << sum << endl; you would type ENDL FOR_REAL sum FOR_REAL COUT;
     // HARD(er) CHALLENGE: sum the numbers between 255 and 1000 but you can only use the "defines" I gave you
     while(i++, J--) {
         // ONLY CHANGE BELOW THIS SECTION, DO NOT CREATE ANY NEW VARIABLES (you can create loops WITHOUT creating new variables though)
+
     }
 
     ENDL FOR_REAL sum FOR_REAL COUT;
